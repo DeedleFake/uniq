6b41705cc2dc82bbf24aebd67d5ab3a3d7933f5b
DeedleFake <deedlefake@users.noreply.github.com>
2017-04-04 10:49:10 -0400

    Convert to iterator-based version.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::iter::{FromIterator};
 use std::env::{args};
 use std::collections::{HashSet};
+use std::collections::hash_map::{RandomState};
 use std::io::{stdin, BufRead};
+use std::hash::{Hash};
+use std::fmt::{Display};
 
 struct Flags {
 	bad: Option<String>,
@@ -33,6 +36,48 @@ impl FromIterator<String> for Flags {
 	}
 }
 
+struct Uniq<T, I> where T: Iterator<Item=I>, I: Hash + Eq + Clone {
+	set: HashSet<I, RandomState>,
+	iter: T,
+}
+
+impl<T, I> Iterator for Uniq<T, I> where T: Iterator<Item=I>, I: Hash + Eq + Clone {
+	type Item = I;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		match self.iter.next() {
+			Some(item) => {
+				if self.set.insert(item.clone()) {
+					return Some(item);
+				}
+				return self.next();
+			},
+
+			None => None,
+		}
+	}
+}
+
+trait UniqIterator<T, I> where T: Iterator<Item=I>, I: Hash + Eq + Clone {
+	fn uniq(self) -> Uniq<T, I>;
+}
+
+impl<T, I> UniqIterator<T, I> for T where T: Iterator<Item=I>, I: Hash + Eq + Clone {
+	fn uniq(self) -> Uniq<T, I> {
+		Uniq{
+			set: HashSet::new(),
+			iter: self,
+		}
+	}
+}
+
+fn panic<T, E>(e: Result<T, E>) -> T where E: Display {
+	match e {
+		Ok(t) => t,
+		Err(err) => panic!("{}", err),
+	}
+}
+
 fn main() {
 	let flags = args().collect::<Flags>();
 	if let Some(ref bad) = flags.bad {
@@ -42,18 +87,8 @@ fn main() {
 		println!("Usage: {}", args().next().unwrap());
 	}
 
-	let mut lines = HashSet::new();
-
 	let stdin = stdin();
-	for line in stdin.lock().lines() {
-		match line {
-			Ok(line) => {
-				if lines.insert(line.clone()) {
-					println!("{}", line);
-				}
-			},
-
-			Err(err) => panic!(err),
-		}
+	for line in stdin.lock().lines().map(panic).uniq() {
+		println!("{}", line);
 	}
 }
